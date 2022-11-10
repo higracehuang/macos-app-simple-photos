ab2e75da8edc3e2ef3a0de72cec0f79188f1b6c9
higracehuang <117108383+higracehuang@users.noreply.github.com>
2022-11-09 21:05:54 -0600

    Initial Commit

---
+++ SimplePhotos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimplePhotos
+//
+//  Created by Le Huang on 11/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimplePhotos/SimplePhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePhotosApp.swift
+//  SimplePhotos
+//
+//  Created by Le Huang on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SimplePhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
